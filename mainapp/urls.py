f5bed5f01a7860888892c7535f736686b4c64461
LaseStar <lase_star@mail.ru>
2022-10-01 19:46:35 +0300

    Add_lesson_4

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,10 +8,13 @@ app_name = MainappConfig.name
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
+    path("courses_list/", views.CoursesListView.as_view(), name="courses_list"),
     path(
-        "news/<int:page>", views.NewsWithPaginatorView.as_view(), name="news_paginator"
+        "courses/<int:pk>/",
+        views.CoursesDetailView.as_view(),
+        name="courses_detail",
     ),
-    path("courses_list/", views.CoursesPageView.as_view(), name="courses_list"),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
     path("login/", views.LoginPageView.as_view(), name="login"),
